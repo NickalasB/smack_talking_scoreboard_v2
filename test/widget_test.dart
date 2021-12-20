c3b92f53d9feb0f44a1d0c7dda2606f647344a9a
Nick Bradshaw <nickalasb@gmail.com>
2021-12-19 23:01:03 -0800

    Create test harness and migrate default app test to use given when then

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:smack_talking_scoreboard_v2/main.dart';
 
+import 'harness.dart';
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Counter increments smoke test', harness((given, when, then) async {
+    await given.pumpMaterialWidget(const MyApp());
+    then.findOneWidget(find.text('0'));
+    then.findNothing(find.text('1'));
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+    await when.userTaps(find.byIcon(Icons.add));
+    await when.pump();
+    then.findOneWidget(find.text('1'));
+    then.findNothing(find.text('0'));
+  }));
 }
