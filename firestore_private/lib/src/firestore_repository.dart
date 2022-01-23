6dc146f2eea962be5eb11c437d7b42660e2e5e8f
Nick Bradshaw <nickalasb@gmail.com>
2022-01-23 00:40:52 -0800

    Add Firestore_repository tests and implementations

---
+++ firestore_private/lib/src/firestore_repository.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firestore_public/firestore_public.dart';
+import 'package:flutter/cupertino.dart';
+
+class FirestoreRepository implements Firestore {
+  FirestoreRepository({
+    FirebaseFirestore? firebaseFirestore,
+    required this.userEmail,
+  }) : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+
+  final FirebaseFirestore _firebaseFirestore;
+  final String userEmail;
+
+  CollectionReference<Game> get _userGamesCollectionRef {
+    return _firebaseFirestore.collection('users').doc(userEmail).collection('games').withConverter<Game>(
+          fromFirestore: (snapshot, _) => Game.fromJson(snapshot.data()!),
+          toFirestore: (model, _) => model.toJson(),
+        );
+  }
+
+  @visibleForTesting
+  DocumentReference<Game>? gameRef;
+
+  @override
+  Future<void> createUserGame(int pin) async {
+    gameRef = _userGamesCollectionRef.doc(pin.toString());
+    gameRef!.set(const Game());
+  }
+
+  @override
+  Future<void> updateP1Score(int score) => gameRef!.update({'p1Score': score});
+
+  @override
+  Future<void> updateP2Score(int score) => gameRef!.update({'p2Score': score});
+
+  @override
+  Future<void> updateP1Name(String name) => gameRef!.update({'p1Name': name});
+
+  @override
+  Future<void> updateP2Name(String name) => gameRef!.update({'p2Name': name});
+
+  @override
+  Future<Game?> fetchGame(int pin) async => (await _userGamesCollectionRef.doc(pin.toString()).get()).data();
+}
--- firestore_private/test/firestore_repository_test.dart
+++ firestore_private/test/firestore_repository_test.dart
@@ -1,12 +1,96 @@
+import 'dart:async';
+
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firestore_private/src/firestore_repository.dart';
+import 'package:firestore_public/firestore_public.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firestore_private/firestore_private.dart';
+import 'fake_firebase.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  setUp(() {
+    Firebase.delegatePackingProperty = FakeFirebase();
+  });
+
+  test('creates Firestore instance internally when not injected', () {
+    expect(() => FirestoreRepository(userEmail: 'anything'), isNot(throwsException));
+  });
+
+  test('createUserGame should work', () async {
+    final fakeFireStore = FakeFirebaseFirestore();
+    final repo = FirestoreRepository(
+      userEmail: 'test@test.com',
+      firebaseFirestore: fakeFireStore,
+    );
+
+    expect(repo.createUserGame(123), completes);
+  });
+
+  test('gameRef should be set when createUserGameCompletes ', () async {
+    final fakeFireStore = FakeFirebaseFirestore();
+    final repo = FirestoreRepository(
+      userEmail: 'test@test.com',
+      firebaseFirestore: fakeFireStore,
+    );
+    expect(repo.gameRef, isNull);
+
+    expect(repo.createUserGame(123), completes);
+
+    expect(repo.gameRef, isNot(isNull));
+    expect(repo.gameRef!.id, '123');
+  });
+
+  test('Should fetch game from pin when fetchGameCalled', () async {
+    final fakeFireStore = FakeFirebaseFirestore();
+    final repo = FirestoreRepository(
+      userEmail: 'test@test.com',
+      firebaseFirestore: fakeFireStore,
+    );
+
+    repo.createUserGame(111);
+
+    expect(await repo.fetchGame(111), const Game());
+  });
+
+  test('Should properly update scores and names', () async {
+    final fakeFireStore = FakeFirebaseFirestore();
+    final repo = FirestoreRepository(
+      userEmail: 'test@test.com',
+      firebaseFirestore: fakeFireStore,
+    );
+
+    repo.createUserGame(111);
+
+    expect(repo.updateP1Name('Nick'), completes);
+    expect(repo.updateP2Name('Todd'), completes);
+    expect(repo.updateP1Score(7), completes);
+    expect(repo.updateP2Score(11), completes);
+
+    expect(
+        await repo.fetchGame(111),
+        const Game(
+          p1Name: 'Nick',
+          p2Name: 'Todd',
+          p1Score: 7,
+          p2Score: 11,
+        ));
+  });
+
+  test('Should throw if updating game before it is created', () async {
+    final fakeFireStore = FakeFirebaseFirestore();
+    final repo = FirestoreRepository(
+      userEmail: 'test@test.com',
+      firebaseFirestore: fakeFireStore,
+    );
+    Object? exception;
+
+    runZonedGuarded(() {
+      expect(repo.updateP1Score(123), throwsA(UnimplementedError()));
+    }, (e, s) {
+      exception = e;
+    });
+
+    expect(exception.toString(), 'Null check operator used on a null value');
   });
 }
